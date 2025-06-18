a0f42d6f351373e22ccc66ee66baf1fc978d1f00
00-hidan-00 <david.taranov@gmail.com>
2025-06-18 18:15:10 +0300

    🧰 🐳 config(docker): update docker config files

--- docker/app/start.sh
+++ docker/app/start.sh
@@ -19,8 +19,12 @@ make migrate
 
 echo "Starting Django server"
 
+# Create a superuser if it does not exist
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@gmail.com', 'admin')"
+
 # Start the server
 python manage.py runserver 0.0.0.0:8000
+
 
 
 
