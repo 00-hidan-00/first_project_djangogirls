85a1b303f0522ea854d2a4262688e558a0b30c8d
00-hidan-00 <david.taranov@gmail.com>
2025-06-18 19:48:11 +0300

    💄 style: refactor admin registration to use @admin.register decorator

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from .models import Post, Comment
 
-admin.site.register(Post)
-admin.site.register(Comment)
+
+# admin.site.register(Post)
+# admin.site.register(Comment)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    model = Comment
