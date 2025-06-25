9642ba713861627655290cb28a685b1ce06e590e
00-hidan-00 <david.taranov@gmail.com>
2025-06-25 19:58:14 +0300

    🛠 chore: init management command generate_comments.py

--- apps/blog/management/commands/generate_comments.py
+++ apps/blog/management/commands/generate_comments.py
@@ -1,4 +1,9 @@
+import logging
+
 from django.core.management.base import BaseCommand, CommandParser
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 class Command(BaseCommand):
