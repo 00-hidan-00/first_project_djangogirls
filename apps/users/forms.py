8387747a51814107c6b184b7e47a3c330c972592
00-hidan-00 <david.taranov@gmail.com>
2025-08-18 21:40:24 +0300

    🐛 fix(users.forms): use custom User model directly in Meta

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,13 +1,12 @@
 from django import forms
-from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
-User = get_user_model()
+from apps.users.models import User
 
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={"class": "input"}))
 
-    class Meta(UserCreationForm.Meta):
+    class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
