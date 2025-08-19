24582883d8ebdf70b7263d195c72445f97387fa6
00-hidan-00 <david.taranov@gmail.com>
2025-08-19 21:11:40 +0300

    ♻️ refactor(favorites): move favorites merge logic from view to signal

--- apps/users/views/__init__.py
+++ apps/users/views/__init__.py
@@ -2,5 +2,3 @@ from apps.users.views.auth.login import UserLoginView  # noqa: F401
 from apps.users.views.auth.password_reset import UserPasswordResetView  # noqa: F401
 from apps.users.views.auth.password_reset_confirm import UserPasswordResetConfirmView  # noqa: F401
 from apps.users.views.auth.signup import SignUpView  # noqa: F401
-from apps.users.views.favorites.favorite_posts import favorite_posts  # noqa: F401
-from apps.users.views.favorites.toggle_favorite import toggle_favorite  # noqa: F401
